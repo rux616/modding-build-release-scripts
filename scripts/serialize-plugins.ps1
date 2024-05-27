46f913b35754bc93878a194547847ea8f616c616
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2024-05-28 07:19:59 +1200

    switch to checking args in the function itself

--- scripts/serialize-plugins.ps1
+++ scripts/serialize-plugins.ps1
@@ -19,14 +19,42 @@
 # serialize plugins via calling Spriggit
 
 param (
-    [string[]] $PluginNames = (Get-ChildItem -Path ".\data\*" -Include "*.esl", "*.esm", "*.esp" -File)
+    [string[]] $PluginNames = (Get-ChildItem -Path ".\data\*" -Include "*.esl", "*.esm", "*.esp" -File).Name,
+    [switch] $JSON,
+    [switch] $YAML,
+    [switch] $Fallout4,
+    [switch] $Starfield
 )
 
+# make sure to stop if an error happens
+$ErrorActionPreference = "Stop"
+
+# since parameter sets are a PITA in PowerShell, we'll just check manually
+if (-not ($JSON -xor $YAML)) {
+    throw "You must specify one of either JSON or YAML as the output format."
+}
+if (-not ($Fallout4 -xor $Starfield)) {
+    throw "You must specify one of either Fallout 4 or Starfield as the game release."
+}
+
+$game_release = if ($Fallout4) { "Fallout 4" } elseif ($Starfield) { "Starfield" }
+$output_format = if ($YAML) { "Spriggit.Yaml" } elseif ($JSON) { "Spriggit.Json" }
+
+# serialize each plugin individually
 $PluginNames | ForEach-Object {
-    &(Join-Path $PSScriptRoot "..\bin\Spriggit\Spriggit.exe") `
-        serialize `
-        --InputPath ".\data\$($_.Name)" `
-        --OutputPath ".\spriggit\$($_.Name)" `
-        --GameRelease Starfield `
-        --PackageName Spriggit.Yaml
+    $plugin_name = $_
+    $spriggit_arguments = @(
+        "serialize"
+        "--InputPath"
+        ".\data\$plugin_name"
+        "--OutputPath"
+        ".\spriggit\$plugin_name"
+        "--GameRelease"
+        $game_release
+        "--PackageName"
+        $output_format
+        "--PackageVersion"
+        "0.18.0"
+    )
+    & (Join-Path $PSScriptRoot "..\bin\SpriggitCLI\Spriggit.CLI.exe") $spriggit_arguments
 }
